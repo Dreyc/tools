573a4e9e6af140cfcf6bfe0cdb75e7329996bd67
Dreyc <59511921+Dreyc@users.noreply.github.com>
2023-12-01 11:59:57 +0100

    Create installer.sh
    Creation of the script

---
+++ installer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Creation du dossier contenant les outils
+mkdir Tools
+cd Tools
+
+##############################
+#           OSINT            #
+##############################
+mkdir OSINT
+cd OSINT
+
+
+cd ..
+##############################
+#           Network          #
+##############################
+mkdir Network
+cd Network
+# -- WIFI --
+mkdir WIFI
+cd WIFI
+
+cd ..
+# -- WIFI --
+
+cd ..
+##############################
+#           Password         #
+##############################
+mkdir Password
+cd Password
+
+cd ..
+##############################
+#           Forensic         #
+##############################
+mkdir Forensic
+cd Forensic
+
+cd ..
+##############################
+#           Exploit          #
+##############################
+mkdir Exploit
+cd Exploit
+
+cd ..
+##############################
+#           Windows          #
+##############################
+mkdir Windows
+cd Windows
+
+cd ..
+##############################
+#           Lists            #
+##############################
+mkdir Lists
+cd Lists
+
+cd ..
+
+cd ..
+echo "INSTALLATION DONE !"
